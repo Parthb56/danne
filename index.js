189cebb8e37fda16ad757e112dce406967b8cb86
Parthb56 <103143256+Parthb56@users.noreply.github.com>
2022-10-15 00:47:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
-var username = data["name"]
+var username = "danne69"
 var nightskip = data["auto-night-skip"]
 const initBot = () => {
 var bot = mineflayer.createBot({
